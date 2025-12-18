4b697070fce5bbc3ce678f6dc4ec9e960e98a3d6
oneureka <65535758+oneureka@users.noreply.github.com>
2025-12-18 23:23:31 +0800

    The Tao programming language

--- lib/tao/parser.rb
+++ lib/tao/parser.rb
@@ -21,6 +21,48 @@ module Tao
     def_delegator :@lexer, :next_token
     def_delegator :@lexer, :tokens
 
+    def synchronize
+      advance
+
+      loop do
+        break if eof?
+        return if previous.type == Token::Semi
+
+        case peek.type
+        when Token::If     then return
+        when Token::Loop   then return
+        when Token::Match  then return
+        when Token::Return then return
+        when Token::Fun    then return
+        when Token::Let    then return
+        when Token::Data   then return
+        else
+          advance
+        end
+      end
+    end
+
+    def consume(type, message)
+      return advance if check?(type)
+      raise ParseError
+    end
+
+    def match?(*types)
+      types.each do |type|
+        if check?(type)
+          advance
+          return true
+        end
+      end
+
+      false
+    end
+
+    def check?(type)
+      return false if eof?
+      peek.type == type
+    end
+
     def advance
       @_peek_token = next_token
       @current += 1 unless eof?
